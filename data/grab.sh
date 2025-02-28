111676e565a0f87cb208a3bb33c9d172a0ede791
dyrnq <dyrnq@qq.com>
2025-02-28 15:37:22 +0800

    Refactoring /data

--- data/grab.sh
+++ data/grab.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/env bash
 
 # <https://github.com/jainabhishek0406/JavaSpark_QueryExample>
-#curl -fSL# -O https://github.com/jainabhishek0406/JavaSpark_QueryExample/raw/master/data/100%20Sales%20Records.csv
-#curl -fSL# -O https://github.com/jainabhishek0406/JavaSpark_QueryExample/raw/master/data/1000%20Sales%20Records.csv
+export https_proxy="http://127.0.0.1:8118"
+curl -fSL# -O https://github.com/jainabhishek0406/JavaSpark_QueryExample/raw/master/data/100%20Sales%20Records.csv
+curl -fSL# -O https://github.com/jainabhishek0406/JavaSpark_QueryExample/raw/master/data/1000%20Sales%20Records.csv
+curl -fSL# -O https://github.com/parmarsachin/spark-dataframe-demo/raw/master/src/main/resources/Sample.csv
+curl -fSL# -O https://github.com/parmarsachin/spark-dataframe-demo/raw/master/src/main/resources/dept.json
+curl -fSL# -O https://github.com/parmarsachin/spark-dataframe-demo/raw/master/src/main/resources/emp.json
+curl -fSL# -O https://github.com/parmarsachin/spark-dataframe-demo/raw/master/src/main/resources/register.json
+curl -fSL# -O https://github.com/parmarsachin/spark-dataframe-demo/raw/master/src/main/resources/sample-data.csv
+curl -fSL# -O https://github.com/parmarsachin/spark-dataframe-demo/raw/master/src/main/resources/sample-data.json
+
 
 for f in *.csv; do
   if [[ -e "$f" ]]; then
