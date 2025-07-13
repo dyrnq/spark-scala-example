c5474dd238da2247b9efb4c92f317c27e072c613
dyrnq <dyrnq@qq.com>
2025-07-13 16:16:01 +0800

    Remove client.credentials-provider

--- src/main/scala/sample/IcebergRestMultiSupportSimple.scala
+++ src/main/scala/sample/IcebergRestMultiSupportSimple.scala
@@ -16,10 +16,12 @@ object IcebergRestMultiSupportSimple {
       .config("spark.sql.catalog.foo_rest_catalog.prefix", "foo")
       .config("spark.sql.catalog.foo_rest_catalog.s3.endpoint", "http://192.168.6.159:9000")
       .config("spark.sql.catalog.foo_rest_catalog.s3.path-style-access", "true")
-      .config("spark.sql.catalog.foo_rest_catalog.client.credentials-provider", "sample.CustomCredentialProvider")
+//      .config("spark.sql.catalog.foo_rest_catalog.client.credentials-provider", "sample.CustomCredentialProvider")
       .config("spark.sql.catalog.foo_rest_catalog.client.region", "us-east-1")
-      .config("spark.sql.catalog.foo_rest_catalog.client.credentials-provider.accessKeyId", "vUR3oLMF5ds8gWCP")
-      .config("spark.sql.catalog.foo_rest_catalog.client.credentials-provider.secretAccessKey", "odWFIZukYrw9dY0G5ezDKMZWbhU0S4oD")
+      .config("spark.sql.catalog.foo_rest_catalog.s3.access-key-id", "vUR3oLMF5ds8gWCP")
+      .config("spark.sql.catalog.foo_rest_catalog.s3.secret-access-key", "odWFIZukYrw9dY0G5ezDKMZWbhU0S4oD")
+//      .config("spark.sql.catalog.foo_rest_catalog.client.credentials-provider.accessKeyId", "vUR3oLMF5ds8gWCP")
+//      .config("spark.sql.catalog.foo_rest_catalog.client.credentials-provider.secretAccessKey", "odWFIZukYrw9dY0G5ezDKMZWbhU0S4oD")
 
 
       .config("spark.sql.catalog.bar_rest_catalog", "org.apache.iceberg.spark.SparkCatalog")
@@ -29,10 +31,12 @@ object IcebergRestMultiSupportSimple {
       .config("spark.sql.catalog.bar_rest_catalog.prefix", "bar")
       .config("spark.sql.catalog.bar_rest_catalog.s3.endpoint", "http://192.168.6.159:9200")
       .config("spark.sql.catalog.bar_rest_catalog.s3.path-style-access", "true")
-      .config("spark.sql.catalog.bar_rest_catalog.client.credentials-provider", "sample.CustomCredentialProvider")
+//      .config("spark.sql.catalog.bar_rest_catalog.client.credentials-provider", "sample.CustomCredentialProvider")
       .config("spark.sql.catalog.bar_rest_catalog.client.region", "us-east-1")
-      .config("spark.sql.catalog.bar_rest_catalog.client.credentials-provider.accessKeyId", "vUR3oLMF5ds8gWCP")
-      .config("spark.sql.catalog.bar_rest_catalog.client.credentials-provider.secretAccessKey", "odWFIZukYrw9dY0G5ezDKMZWbhU0S4oD")
+      .config("spark.sql.catalog.bar_rest_catalog.s3.access-key-id", "vUR3oLMF5ds8gWCP")
+      .config("spark.sql.catalog.bar_rest_catalog.s3.secret-access-key", "odWFIZukYrw9dY0G5ezDKMZWbhU0S4oD")
+//      .config("spark.sql.catalog.bar_rest_catalog.client.credentials-provider.accessKeyId", "vUR3oLMF5ds8gWCP")
+//      .config("spark.sql.catalog.bar_rest_catalog.client.credentials-provider.secretAccessKey", "odWFIZukYrw9dY0G5ezDKMZWbhU0S4oD")
       .getOrCreate()
 
 
@@ -40,6 +44,7 @@ object IcebergRestMultiSupportSimple {
     spark.sql("SHOW CATALOGS").show()
     spark.sql("SHOW DATABASES FROM foo_rest_catalog").show()
     spark.sql("SHOW DATABASES FROM bar_rest_catalog").show()
+//    spark.sql("select * from bar_rest_catalog.bar.bar_table;")
     spark.sql(
       """
         |SELECT * FROM
